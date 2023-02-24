78f69209b76ebe5858e73db0654cc8e1ac5c763c
PaKu <cookie4rent@gmail.com>
2023-02-24 16:13:15 +0100

    Start admin periods

---
+++ js/aplan_moderation_periods.js
@@ -0,0 +1,15 @@
+import {get_ajax} from './ajax.js';
+
+export function init_periods() {
+  document.getElementById('moderation_periods_new_button').addEventListener('click', create_new);
+  document.getElementById('moderation_periods_edit_save_button').addEventListener('click', save_entry);
+  document.getElementById
+}
+
+function create_new() {
+
+}
+
+function save_entry() {
+
+}
